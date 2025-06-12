8541306021502fc59c6854fae989a4fc51658b59
Rafy Prappo <rafy.prappo01@gmail.com>
2025-06-13 01:52:22 +0600

    hello

--- C#/Step_3_Input.cs
+++ C#/Step_3_Input.cs
@@ -1,17 +1,17 @@
-// using System;
+using System;
 
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.WriteLine("Please input your name: ");
-//         string name = Console.ReadLine();
-//         Console.WriteLine("Please input your age: ");
-//         int age = Convert.ToInt32(Console.ReadLine());
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Please input your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
 
-//         Console.WriteLine($"Hello {name}! You are {age} years old.\nAre you doing well?");
+        Console.WriteLine($"Hello {name}! You are {age} years old.\nAre you doing well?");
 
-//     }
-// }
+    }
+}
 
-// // Had to comment because it won't work if theres the same main function in the same program folder
+// Had to comment because it won't work if theres the same main function in the same program folder
